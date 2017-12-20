d538bdffa3bcc048ff90f9ba902f7f5a435eeabc
Bas Harenslak <basharenslak@godatadriven.com>
2017-12-20 20:56:07 +0100

    Moved fetching of ticker prices to seperate func

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 var (
 	debug = kingpin.Flag("debug", "Enable debug mode.").Short('d').Default("false").Bool()
 	addr  = kingpin.Flag("address", "The address to listen on for HTTP requests").Short('a').Default(":8080").String()
+	pairs = kingpin.Flag("pairs", "Pairs to fetch from Kraken").Short('p').Default("XXBTZEUR").String()
 
 	openingPrices = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "opening_prices",
@@ -34,31 +35,33 @@ func init() {
 	prometheus.MustRegister(totalBalance)
 }
 
+func fetchKrakenPrices() {
+	key := os.Getenv("KEY")
+	secret := os.Getenv("SECRET")
+	api := krakenapi.New(key, secret)
+
+	//krakenapi.PairTickerInfo{}
+
+	for {
+		ticker, err := api.Ticker(krakenapi.XXBTZEUR)
+		if err != nil {
+			if ticker == nil {
+				log.Warning("Result was empty. Prices not updated.")
+			} else {
+				log.Fatal(err)
+			}
+		} else {
+			askPrice, _ := strconv.ParseFloat(ticker.XXBTZEUR.Ask[0], 64)
+			openingPrices.WithLabelValues("XXBTZEUR").Set(askPrice)
+		}
+
+		time.Sleep(time.Duration(5 * time.Second))
+	}
+}
+
 func main() {
 	kingpin.Parse()
-
-	go func() {
-		key := os.Getenv("KEY")
-		secret := os.Getenv("SECRET")
-		api := krakenapi.New(key, secret)
-
-		for {
-			ticker, err := api.Ticker(krakenapi.XXBTZEUR)
-			if err != nil {
-				if ticker == nil {
-					log.Warning("Result was empty. Prices not updated.")
-				} else {
-					log.Fatal(err)
-				}
-			} else {
-				askPrice, _ := strconv.ParseFloat(ticker.XXBTZEUR.Ask[0], 64)
-				openingPrices.WithLabelValues("XXBTZEUR").Set(askPrice)
-			}
-
-			time.Sleep(time.Duration(5 * time.Second))
-		}
-	}()
-
+	go fetchKrakenPrices()
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
